9ef1781d1e8f805f4e82065e0caebb8577f5c1ad
Fred77DeLaMut <augerfetm@aol.com>
2023-09-20 10:06:47 +0100

    fonction initJs

--- js/index.js
+++ js/index.js
@@ -1,4 +1,7 @@
-var jsLoadedNode=document.querySelector('#is-js-loaded')
-jsLoadedNode.innerHTML='JS <b>Chargé</b>'
-jsLoadedNode.style.color="green"
-jsLoadedNode.style.textAlign="center"
+function initJs() {
+    var jsLoadedNode = document.querySelector("#is-js-loaded");
+    jsLoadedNode.innerHTML = "JS <b>Chargé</b>";
+    jsLoadedNode.style.color = "lightgreen";
+    jsLoadedNode.style.textAlign = "center";    
+}
+initJs();
