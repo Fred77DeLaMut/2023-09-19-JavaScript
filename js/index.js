c5d355b4dd5a43683a19806088859d8a2cd2ebeb
Fred77DeLaMut <augerfetm@aol.com>
2023-09-20 10:22:03 +0100

    on supprime les logs

--- js/index.js
+++ js/index.js
@@ -1,14 +1,7 @@
 function initJs(color) {
-    console.time('fnInitJs');
-    console.log('du texte','autre chose',color);
-    console.log(color);
-    console.warn(color);
-    console.error(color);
-    console.trace(arguments);
     var jsLoadedNode = document.querySelector("#is-js-loaded");
     jsLoadedNode.innerHTML = "JS <b>Chargé</b>";
     jsLoadedNode.style.color = color;
     jsLoadedNode.style.textAlign = "center";    
-    console.timeEnd('fnInitJs');
 }
 initJs('aquamarine',{a:34, b:10});
