99d7bb7139642ddc21294644eb04292150586c53
Fred77DeLaMut <augerfetm@aol.com>
2023-09-20 10:30:42 +0100

    documentation du code

--- js/index.js
+++ js/index.js
@@ -1,7 +1,12 @@
+/**
+ * fonction d'initialisation du js
+ * @param {string} color chaine de valeur de couleur CSS
+ * @returns {undefined} pas de retour
+ */
 function initJs(color) {
     var jsLoadedNode = document.querySelector("#is-js-loaded");
     jsLoadedNode.innerHTML = "JS <b>Chargé</b>";
     jsLoadedNode.style.color = color;
     jsLoadedNode.style.textAlign = "center";    
 }
-initJs('aquamarine',{a:34, b:10});
+initJs('aquamarine');
