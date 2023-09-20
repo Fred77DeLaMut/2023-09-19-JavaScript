c61c4db35b8a53e5ca43347a9f58c4089369c196
Fred77DeLaMut <augerfetm@aol.com>
2023-09-20 09:27:40 +0100

    Premier script JS

--- js/index.js
+++ js/index.js
@@ -1,1 +1,4 @@
-confirm('coucou')
+var jsLoadedNode=document.querySelector('#is-js-loaded')
+jsLoadedNode.innerHTML='JS <b>Chargé</b>'
+jsLoadedNode.style.color="green"
+jsLoadedNode.style.textAlign="center"
