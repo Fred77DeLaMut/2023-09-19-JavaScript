b1a5a4da2c55c80c21aa69c2cc7169933e7e83d9
Fred77DeLaMut <augerfetm@aol.com>
2023-09-20 10:17:22 +0100

    Les logs sur la console

--- js/index.js
+++ js/index.js
@@ -1,7 +1,12 @@
-function initJs() {
+function initJs(color) {
+    console.log('du texte');
+    console.log(color);
+    console.warn(color);
+    console.error(color);
+    console.trace(color);
     var jsLoadedNode = document.querySelector("#is-js-loaded");
     jsLoadedNode.innerHTML = "JS <b>Chargé</b>";
-    jsLoadedNode.style.color = "lightgreen";
+    jsLoadedNode.style.color = color;
     jsLoadedNode.style.textAlign = "center";    
 }
-initJs();
+initJs('aquamarine');
