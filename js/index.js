b2cc9ebf6c9c7baffb66534dd0d2066e2f218dda
Fred77DeLaMut <augerfetm@aol.com>
2023-09-20 12:29:38 +0100

    Le DOMContentLoaded

--- js/index.js
+++ js/index.js
@@ -1,3 +1,8 @@
+/**
+ * affichage de l'état du js
+ * @param {string} color chaine de valeur de couleur CSS
+ * @return {undefined} pas de retour
+ */
 function ChangePreHeader(color) {
   var jsLoadedNode = document.querySelector("#is-js-loaded");
   jsLoadedNode.innerHTML = "JS <b>Chargé</b>";
@@ -6,14 +11,20 @@ function ChangePreHeader(color) {
 }
 
 /**
- * fonction d'initialisation du js
+ * fonction principale d'initialisation du js
  * @param {string} color chaine de valeur de couleur CSS
  * @returns {undefined} pas de retour
  */
 function initJs(color) {
+  var bt = document.querySelector("#ne-pas-cliquer");
   ChangePreHeader(color);
-  document.querySelector('button#ne-pas-cliquer').addEventListener('click',function(evt){
+  bt.addEventListener("click", function (evt) {
     console.log(evt);
-  })
+    console.log(evt.target);
+    bt.innerHTML = "Trop tard";
+    ChangePreHeader("black");
+  });
 }
+document.addEventListener('DOMContentLoaded',function(evt){
 initJs("aquamarine");
+});
