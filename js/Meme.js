086f5ea54671bc61da6f2d759c78ae5deaa7ff09
Fred77DeLaMut <augerfetm@aol.com>
2023-09-22 09:17:40 +0100

    modification de l'objet meme sur modification des input

--- js/Meme.js
+++ js/Meme.js
@@ -1,10 +1,10 @@
-import { ImageMeme as Img} from "./Image.js";
+import { ImageMeme as Img } from "./Image.js";
 /**
  * Constructeur d'objet Meme avec ou sans json de base
  */
 export function Meme(jsonConfiguredMemeStr) {
   // this attache la valeur à l'objet et est donc visible de l'extérieur
-  this.texte = "Texte du meme";
+  this.texte = "C'est vide";
   this.x = 0;
   this.y = 32;
   this.color = "#000000";
@@ -13,7 +13,23 @@ export function Meme(jsonConfiguredMemeStr) {
   this.underline = false;
   this.italic = false;
   this.imageId = -1;
-  this.image=new Img;
+  this.image = new Img();
+  this.render = undefined;
+
+  const insideRender = () => {
+    if (undefined !== this.render && typeof this.render === "function") {
+      this.render(this);
+    }
+  };
+
+  /**
+   * update d'un meme par objet avec force render on update
+   * @param {object} memeData
+   */
+  this.update = function (memeData) {
+    Object.assign(this, memeData);
+    insideRender();
+  };
 
   /**
    * chargement de valeurs a partir d'un meme en json
@@ -23,10 +39,9 @@ export function Meme(jsonConfiguredMemeStr) {
     Object.assign(this, JSON.parse(jsonStr));
   };
 
-  if (jsonConfiguredMemeStr!==undefined) {
+  if (jsonConfiguredMemeStr !== undefined) {
     this.loadFromString(jsonConfiguredMemeStr);
-    
   }
 }
 
-var meme = new Meme();
+// var meme = new Meme();
--- js/index.js
+++ js/index.js
@@ -1,7 +1,50 @@
 import { Meme } from "./Meme.js";
 import { ImagesList, listeImages } from "./Image.js";
-/*let meme=new Meme();
-console.log(meme);*/
+
+const addFormEvents = () => {
+  /**
+   * fonction de gestion de soumission formulaire
+   * @param {SubmitEvent} evt event de soumission
+   */
+  function onformsubmit(evt) {
+    evt.preventDefault();
+    console.log(current);
+  }
+  const form = document.forms["meme_form"];
+  form.addEventListener("submit", onformsubmit);
+  // création d'un évènement qui met à jour
+  form["texte"].addEventListener("input", (evt) => {
+    current.update({ texte: evt.target.value });
+  });
+  form["valx"].addEventListener("input", (evt) => {
+    current.update({ x: evt.target.value });
+  });
+  form["valy"].addEventListener("input", (evt) => {
+    current.update({ y: evt.target.value });
+  });
+  form["coul"].addEventListener("input", (evt) => {
+    current.update({ color: evt.target.value });
+  });
+  form["fontSize"].addEventListener("input", (evt) => {
+    current.update({ fontSize: evt.target.value });
+  });
+  form["fontWeight"].addEventListener("input", (evt) => {
+    current.update({ fontWeight: evt.target.value });
+  });
+};
+
+/**
+ * la fameuse fonction qui va effectuer le render
+ * @param {*} mem
+ */
+const renderMeme = (mem) => {
+  console.log(mem);
+  /* rendu dom pour un meme */
+};
+
+let current = new Meme();
+current.render = renderMeme;
+//console.log(current);
 
 /**
  * chargement de la liste des options du select
@@ -47,34 +90,7 @@ function initJs(color) {
     bt.innerHTML = "Trop tard";
     ChangePreHeader("black");
   });
-
-  /**
-   * fonction de gestion de soumission formulaire
-   * @param {SubmitEvent} evt event de soumission
-   */
-  function onformsubmit(evt) {
-    evt.preventDefault();
-    console.log(evt);
-    var meme = {
-      texte: evt.target["texte"].value,
-      x: Number(evt.target["valx"].value),
-      y: Number(evt.target["valy"].value),
-      color: evt.target["color"].value,
-      fontSize: Number(evt.target["fontSize"].value),
-      fontWeight: evt.target["fontWeight"].value,
-    };
-    console.log(meme);
-    // console.log("texte=", evt.target["texte"].value);
-    // console.log("x=", evt.target["valx"].value);
-    // console.log("y=", evt.target["valy"].value);
-    // console.log("color=", evt.target["color"].value);
-    // console.log("fontSize=", evt.target["fontSize"].value);
-    // console.log("fontWeight=", evt.target["fontWeight"].value);
-    //debugger; force l'arrêt. pas beau
-  }
-
-  document.forms["meme_form"].addEventListener("submit", onformsubmit);
-  // on peut aussi écrire document.forms.meme_form
+  addFormEvents();
 }
 
 //déclenchement du chargement des images
